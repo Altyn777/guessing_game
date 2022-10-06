5b5d38d6f2c4c5c95729a8f08a220bfdd8243c98
Altyn777 <darzeeia@gmail.com>
2022-10-06 21:58:20 +0300

    start

---
+++ readme.md
@@ -0,0 +1,5 @@
+To create project:
+`cargo init --vcs=git`
+
+To build and run:
+`cargo run`
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+// prelude
+// io input/output library that comes from the standard library std
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+}
